e3cb3c138666de95d90f43c9e820c4a10431c374
Robson Ferreira <robsondmf1234@gmail.com>
2020-06-22 22:25:09 -0300

    Commit Incial
    Primeiro commit

---
+++ app/src/main/java/com/example/desafio/model/FilmeItem.kt
@@ -0,0 +1,8 @@
+package com.example.desafio.model
+
+data class FilmeItem( val imagemCapa: Int,
+                      val nota: String,
+                      val titulo: String,
+                      val categoria: String,
+                      val paisFilme: String,
+                      val descricao: String)
