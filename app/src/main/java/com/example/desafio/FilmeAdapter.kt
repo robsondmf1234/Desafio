80c1eee8e8ff7eb763f1dc33767bcc05c4ea19aa
Robson Ferreira <robsondmf1234@gmail.com>
2020-06-24 22:27:38 -0300

    Clique no recyclerview
    #2 Adicioando clique no recylerview

--- app/src/main/java/com/example/desafio/FilmeAdapter.kt
+++ app/src/main/java/com/example/desafio/FilmeAdapter.kt
@@ -7,8 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import com.example.desafio.R
 import com.example.desafio.model.FilmeItem
 import kotlinx.android.synthetic.main.list_item.view.*
 
@@ -19,13 +19,14 @@ class FilmeAdapter(val listaFilmes: List<FilmeItem>, val context: Context) :
         val filmeView =
             LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
 
-        return FilmeViewHolder(filmeView)
+        return FilmeViewHolder(filmeView,context)
     }
 
 
     override fun onBindViewHolder(holder: FilmeViewHolder, position: Int) {
         val filmeAtual = listaFilmes[position]
 
+        //holder.itemView.setOnClickListener()
         holder.imageViewCapa.setImageResource(filmeAtual.imagemCapa)
         holder.textViewNota.text = filmeAtual.nota
         holder.textViewTitulo.text = filmeAtual.titulo
@@ -39,12 +40,16 @@ class FilmeAdapter(val listaFilmes: List<FilmeItem>, val context: Context) :
         return listaFilmes.size
     }
 
-    class FilmeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class FilmeViewHolder(itemView: View,context: Context) : RecyclerView.ViewHolder(itemView) {
         val imageViewCapa: ImageView = itemView.imageCapaFilme
         val textViewNota: TextView = itemView.textNotaFilme
         val textViewTitulo: TextView = itemView.textTituloFilme
         val textViewCategoria: TextView = itemView.textCategoria
         val textViewPais: TextView = itemView.textPais
         val textViewDescricao: TextView = itemView.textDescricao
+        //Adicionando clique ao recyclerview
+        val clique = itemView.setOnClickListener({
+            Toast.makeText(context, "O ${textViewTitulo} foi clicado", Toast.LENGTH_SHORT).show()
+        })
     }
 }
--- app/src/main/java/com/example/desafio/model/FilmeItem.kt
+++ app/src/main/java/com/example/desafio/model/FilmeItem.kt
@@ -1,6 +1,7 @@
 package com.example.desafio.model
 
-data class FilmeItem( val imagemCapa: Int,
+data class FilmeItem( //val id:Int
+    val imagemCapa: Int,
                       val nota: String,
                       val titulo: String,
                       val categoria: String,
