cedd960a483171af5e34270d65e8e147d0bd6c89
Robson Ferreira <robsondmf1234@gmail.com>
2020-07-09 16:33:05 -0300

    #17 Adicionado SearchVew no xml
    Adicionado o searchView no xml do FragmentSeacrh

--- app/src/main/java/com/example/desafio/fragment/FragmentSearch.kt
+++ app/src/main/java/com/example/desafio/fragment/FragmentSearch.kt
@@ -2,7 +2,9 @@ package com.example.desafio.fragment
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -17,7 +19,6 @@ import kotlin.collections.ArrayList
 
 class FragmentSearch : Fragment() {
 
-
     val displayList = ArrayList<Filme>()
     var arrayList = Mock().getListaFilmes()
 
@@ -26,70 +27,56 @@ class FragmentSearch : Fragment() {
 
         displayList.addAll(arrayList)
 
-        val myAdapter = FilmeAdapter2(displayList,context!!,this::abreSegundaTela)
+        val myAdapter = FilmeAdapter2(displayList, context!!, this::abreSegundaTela)
 
         recycler_fragment_search.layoutManager = LinearLayoutManager(context)
         recycler_fragment_search.adapter = myAdapter
+
+        buscaSearchView(search_title)
     }
 
-    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false)
-
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setHasOptionsMenu(true)
         super.onCreate(savedInstanceState)
     }
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu_search,menu)
-        val menuItem = menu.findItem(R.id.search)
 
-        if (menuItem!= null){
-            val searchView = menuItem.actionView as SearchView
+    private fun abreSegundaTela(filme: Filme) {
+          val vaiPraProximaTela = Intent(context, TelaSecundaria::class.java)
+           vaiPraProximaTela.putExtra("filme",filme)
+           startActivity(vaiPraProximaTela)
 
-            searchView.setOnQueryTextListener(object :SearchView.OnQueryTextListener{
-                override fun onQueryTextSubmit(query: String?): Boolean {
-                    TODO("Not yet implemented")
-                }
+    }
 
-                override fun onQueryTextChange(newText: String?): Boolean {
-                    if (newText!!.isNotEmpty()){
-                        displayList.clear()
-                        val search = newText.toLowerCase(Locale.getDefault())
-                        arrayList.forEach {
-                            if (it.titulo.toLowerCase(Locale.getDefault()).contains(search)){
-                                displayList.add(it)
-                            }
+    //Método para buscar os filmes pelo SearchView
+    fun buscaSearchView(search_title: SearchView) {
+        search_title.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText!!.isNotEmpty()) {
+                    displayList.clear()
+                    val search = newText.toLowerCase(Locale.getDefault())
+                    arrayList.forEach {
+                        if (it.titulo.toLowerCase(Locale.getDefault()).contains(search)) {
+                            displayList.add(it)
                         }
-                        recycler_fragment_search.adapter!!.notifyDataSetChanged()
                     }
-                    else{
-                        displayList.clear()
-                        displayList.addAll(arrayList)
-                        recycler_fragment_search.adapter!!.notifyDataSetChanged()
-                    }
-                    return true
+                    recycler_fragment_search.adapter!!.notifyDataSetChanged()
+                } else {
+                    displayList.clear()
+                    displayList.addAll(arrayList)
+                    recycler_fragment_search.adapter!!.notifyDataSetChanged()
                 }
+                return true
+            }
 
-            })
-
-
-        }
-
-        super.onCreateOptionsMenu(menu, inflater)
+        })
     }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return super.onOptionsItemSelected(item)
-    }
-    //Método para passado no parametro para capturar clique no itemview dentro do recyclerview
-    private fun abreSegundaTela(filme: Filme){
-        val vaiPraProximaTela = Intent(context, TelaSecundaria::class.java)
-        vaiPraProximaTela.putExtra("filme",filme)
-        startActivity(vaiPraProximaTela)
-
-    }
-
 }
